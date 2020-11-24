f6636bbbe8bca68ee107909c266be00eb57f03a5
jinah0623 <jinah3579@yahoo.co.kr>
2020-11-24 16:49:25 +0900

    11/24

--- photo/js/photo.js
+++ photo/js/photo.js
@@ -4,6 +4,7 @@ class Photo {
     constructor() {
         /*left side*/
         this.aside = document.querySelector('.aside');
+        this.aside1 = document.querySelector('.aside1');
         this.bside = document.querySelector('.bside');
         this.cside = document.querySelector('.cside');
         /*center*/
@@ -13,6 +14,9 @@ class Photo {
         /*right side*/
         this.dside = document.querySelector('.dside');
         this.eside = document.querySelector('.eside');
+        this.xbtn = document.querySelector('.xbtn');
+
+        this.shadow = document.querySelector('.shadow');
 
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
@@ -38,6 +42,9 @@ class Photo {
         this.divPosition(this.footer, `${this.y*3}px`,`${this.x}px`);
         this.divPosition(this.dside, `-${this.y}px`,`${this.x*1.93}px`);
         this.divPosition(this.eside, `${this.y*3.1}px`,`${this.x*1.93}px`);
+        this.divPosition(this.xbtn, `${this.center_y}px`,`${this.center_x*1.8}px`);
+  
+
         // this.eside.style.left = `${x*1.93}px`;
         /*aside*/
         this.cssProcess(this.aside,'visible', 'relative', 'transform 2000ms ease-in', `translateY(${this.y}px)`, '1');
@@ -45,9 +52,10 @@ class Photo {
         this.cssProcess(this.cside,'visible', 'relative', 'transform 2000ms ease-in', `translateY(-${this.y}px)`, '1');
         this.cssProcess(this.head, 'visible', 'absolute', 'transform 1400ms ease-in', `translateY(${this.y}px)`, '1' );
         this.cssProcess(this.center, "visible", 'absolute', 'transform 1400ms ease-in', `translate(-${this.x*2}px, -${(this.y*2)+((this.y*35)/100)}px)`, '1');
-        this.cssProcess(this.footer, "visible", 'absolute', 'transform 1800ms ease-in', `translateY(-${this.y+((this.y*10)/100)}px)`, '-1' );
-        this.cssProcess(this.dside,'visible', 'absolute', 'transform 2000ms ease-in', `translateY(${this.y}px)`, '-1');
-        this.cssProcess( this.eside, 'visible', 'absolute', 'transform 1500ms ease-in', `translateY(-${this.y*2}px)`, '-1');
+        this.cssProcess(this.footer, "visible", 'absolute', 'transform 1800ms ease-in', `translateY(-${this.y+((this.y*10)/100)}px)`, '1' );
+        this.cssProcess(this.dside,'visible', 'absolute', 'transform 2000ms ease-in', `translateY(${this.y}px)`, '1');
+        this.cssProcess(this.eside, 'visible', 'absolute', 'transform 1500ms ease-in', `translateY(-${this.y*2}px)`, '1');
+
     }
 
     divPosition(location,top,left){
@@ -80,8 +88,10 @@ class Photo {
         const y = this.stageHeight / 3;
 
         const center = this.center
+        const shadow = this.shadow;
         const center1 = document.querySelector('.center1');
-        const shadow = document.querySelector('.shadow');
+
+        const xbtn = this.xbtn;
 
         center1.addEventListener('click', function(){
             center1.style.background = 'url("img/mackbook.jpg")';
@@ -95,7 +105,16 @@ class Photo {
             shadow.style.display = 'block';
             center.style.zIndex = '99';
             center.style.top= y*3.2+'px';
+
+            xbtn.style.visibility = 'visible';
+            xbtn.style.transition = 'transform 2000ms ease-in';
+            xbtn.style.transform = 'rotate(270deg)';
         })
+        xbtn.addEventListener('click', () => {
+            this.xbtn.style.visibility ='hidden'
+            location.reload();
+        })
+            
     }
     aside1Event(){
         const stageWidth = document.body.clientWidth;
@@ -103,16 +122,60 @@ class Photo {
         const x = this.stageWidth / 3;
         const y = this.stageHeight / 3;
         
-        const aside = this.aside
+        const center1 = this.center1;
+        const aside = this.aside;
+        const shadow = this.shadow;
+        const xbtn = this.xbtn;
         const aside1 = document.querySelector('.aside1');
+        console.log(aside1);
+        const center_x = this.stageWidth / 2;
+        const center_y = this.stageHeight / 2;
 
         aside1.addEventListener('click', function(){
             aside1.style.backgroundSize = 'cover';
             aside1.style.transition = 'transform 1000ms ease-in';
             aside1.style.transform = 'scale(2)';
             aside1.style.fontSize = '50px';
+            aside1.style.textAlign = 'center';
+            aside.style.zIndex = '98'
+            aside1.style.position = 'absolute';
+            shadow.style.display = 'block';
+            aside1.style.left = `${center_x}px`;
+            aside1.style.top = `${center_y}px`;
+            console.log(center_y);
+
+            
+            aside.style.visibility = 'hidden';
+            aside1.style.visibility = 'visible';
+
+            xbtn.style.visibility = 'visible';   
+            xbtn.style.transition = 'transform 2000ms ease-in';
+            xbtn.style.transform = 'rotate(270deg)';
+
+            aside1.addEventListener('click', ()=> {
+                location.href = "../realphone/realphone.html";
+            })
+
+
+        })
+        xbtn.addEventListener('click', () => {
+            this.xbtn.style.visibility ='hidden'
+            location.reload();
         })
     }
+    //app img changing
+    // changeimg(){
+    //     const images = [
+    //     'url("img/music.jpg")',
+    //     'url("img/icloud.jpg")',
+    //     'url("img/msg.jpg")',
+    //     ]
+    //     const apps = document.querySelector('.dside1')
+    //     const bg = images[Math.floor(Math.random()*images.length)];
+    //     apps.style.backgroundImage = bg;
+    //         setInterval(changeimg, 1000)
+    // }
+
 }
 window.onload = () => {
     new Photo();
